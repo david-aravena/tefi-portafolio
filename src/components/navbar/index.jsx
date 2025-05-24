7eb492327c48cdae7e1d0da0bbbfd3145678f592
david-aravena <david.aravenap@gmail.com>
2025-05-24 15:39:25 -0400

    initial state

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,21 @@
+import menuIcon from './svg/menu.svg'
+
+export default function Navbar(){
+  return(
+    <>
+      <div className="navbarContainer">
+        <div className="animatedLogoContainer">
+          <img src={menuIcon} alt="" />
+        </div>
+
+        <div className="bodyContainer">
+
+        </div>
+        
+        <div className="menuContainer"> 
+
+        </div>
+      </div>
+    </>
+  )
+}
