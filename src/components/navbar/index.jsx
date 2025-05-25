f1ca8c0e88ff67a2e79089f9b49d15e5c45fea02
david-aravena <david.aravenap@gmail.com>
2025-05-25 00:37:58 -0400

    add: Navbar aditional

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,24 +1,44 @@
-import menuIcon from './svg/menu.svg'
+import menuIconWhite from './svg/menuWhite.svg'
+import menuIconBlack from './svg/menuBlack.svg'
 import './navbar.css'
 
 export default function Navbar(){
   return(
     <>
-      <div className="navbarContainer">
-        <div className="animatedLogoContainer">
-          <div className="loading">
+      <div className="navbarContainerBlack">
+        <div className="animatedLogoContainerTc">
+          <div className="loadingTc">
 
           </div>
         </div>
         
         <div className="menuContainer">
           <div className="hamburguerContainer"> 
-            <img src={menuIcon} alt="" />
+            <img src={menuIconWhite} alt="" />
           </div>
           <div className="linksContainer">
-            <p>About</p>
-            <p>Contact</p>
-            <p>Home</p>
+            <p style={{color:"white"}}>About</p>
+            <p style={{color:"white"}}>Contact</p>
+            <p style={{color:"white"}}>Home</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="navbarContainerWhite">
+        <div className="animatedLogoContainerDevsafio">
+          <div className="loadingDevsafio">
+
+          </div>
+        </div>
+        
+        <div className="menuContainer">
+          <div className="hamburguerContainer"> 
+            <img src={menuIconBlack} alt="" />
+          </div>
+          <div className="linksContainer">
+            <p style={{color:"black"}}>About</p>
+            <p style={{color:"black"}}>Contact</p>
+            <p style={{color:"black"}}>Home</p>
           </div>
         </div>
       </div>
