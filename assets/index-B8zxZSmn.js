cd0e1572028ba485dec6d4746cdb763673a92e8a
david-aravena <david.aravenap@gmail.com>
2025-05-30 00:42:48 -0400

    Updates

--- assets/index-B8zxZSmn.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CX-RHuDM.js";function r(){return e.jsx("h2",{children:"Home"})}export{r as default};
---
+++ assets/index-DNGtjglK.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Bkcr2IZA.js";function r(){return e.jsx("h2",{children:"Home"})}export{r as default};
